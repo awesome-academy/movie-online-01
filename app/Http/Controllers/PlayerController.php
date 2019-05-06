b63316746f6a685999261a4dbc3e1dbf36e6dee0
hungtb95 <hung95ictu@gmail.com>
2019-05-06 22:22:46 +0700

    refs task #9730_part2 Episode Management

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -12,29 +12,29 @@ use App\Http\Requests\CommentRequest;
 
 class PlayerController extends Controller
 {
-	public function showEpisodes($id, $slug)
-	{
-		$epBySlug = Episode::with('film')->where('film_id', $id)->where('slug', $slug)->firstOrFail();
-		$comments = Comment::with('user')->where('film_id', $id)->orderBy('created_at', 'DESC')->get();
-		$epOfFilm = Film::findOrFail($id)->episodes;
-		$film = Film::findOrFail($id);
+    public function showEpisodes($id, $slug)
+    {
+        $epBySlug = Episode::with('film')->where('film_id', $id)->where('slug', $slug)->firstOrFail();
+        $comments = Comment::with('user')->where('film_id', $id)->orderBy('created_at', 'DESC')->get();
+        $epOfFilm = Film::findOrFail($id)->episodes()->orderBy('slug')->get();
+        $film = Film::findOrFail($id);
 
-		return view('client.player', compact('epBySlug', 'epOfFilm', 'comments', 'film'));
-	}
+        return view('client.player', compact('epBySlug', 'epOfFilm', 'comments', 'film'));
+    }
 
-	public function comment(CommentRequest $request)
-	{
-		$user = Auth::user();
-		Comment::create(
-			[
-				'user_id' => $user->id,
-				'film_id' => $request->film_id,
-				'content' => $request->comment,
-				'created_at' => Carbon::now(),
-				'updated_at' => Carbon::now(),
-			]
-		);
+    public function comment(CommentRequest $request)
+    {
+        $user = Auth::user();
+        Comment::create(
+            [
+                'user_id' => $user->id,
+                'film_id' => $request->film_id,
+                'content' => $request->comment,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]
+        );
 
-		return redirect()->back();;
-	}
+        return redirect()->back();;
+    }
 }
