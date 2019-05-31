bafaa2ab8b007c2dc0eedfd7d9c03acb685ccca1
nguyenthithu1612 <nguyenthubk97@gmail.com>
2019-05-31 16:34:30 +0700

    refs task #9734 upload film to driver

--- database/factories/FilmFactory.php
+++ database/factories/FilmFactory.php
@@ -11,7 +11,7 @@ $factory->define(Film::class, function (Faker $faker) {
         'title_vn' => $faker->realText($maxNbChars = 20),
         'thumb' => $faker->imageUrl($width = 640, $height = 480),
         'director' => $faker->name,
-        'country_id' => $faker->numberBetween($min = 1, $max = 20),
+        'country_id' => $faker->numberBetween($min = 10, $max = 20),
         'year' => $faker->year($max = 'now'),
         'duration' => $faker->numberBetween($min = 120, $max = 200),
         'description' => $faker->text($maxNbChars = 500),
